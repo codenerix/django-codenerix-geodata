ac8a6eb63894626c0c87662c4bebbd2a58d82e39
Juanmi Taboada <juanmi@juanmitaboada.com>
2020-01-09 19:22:26 +0100

    Percent for geodata population process

--- codenerix_geodata/management/commands/populate_geodata.py
+++ codenerix_geodata/management/commands/populate_geodata.py
@@ -25,6 +25,8 @@ Two different databases were used:
     - GeoLite2 Country: http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
 """
 
+import sys
+import time
 from os.path import dirname, join
 from csv import reader
 
@@ -33,6 +35,7 @@ from django.core.management.base import BaseCommand
 from django.utils.translation import ugettext as _
 from django.core.exceptions import ObjectDoesNotExist
 
+from codenerix_lib.debugger import Debugger
 from codenerix_extensions.helpers import FileBZ2
 from codenerix_geodata.models import Continent, Country, Region, Province, City, TimeZone
 
@@ -148,10 +151,46 @@ def city_lines(filename):
                 yield int(geoid), country_code, region_code, province_code, clean(city_name), clean(time_zone)
 
 
-class Command(BaseCommand):
+class Command(BaseCommand, Debugger):
     help = _('Populates Continent, Country and City models')
+    __percent = None
+
+    def percent_init(self, text, total):
+        self.__percent = {}
+        self.__percent['text'] = text
+        self.__percent['last'] = time.time()-6
+        self.__percent['counter'] = 0
+        self.__percent['total'] = total
+        self.percent()
+
+    def percent(self):
+        last = (self.__percent['counter']+1) == self.__percent['total']
+        now = time.time()
+        if (self.__percent['last']+6<now) or last:
+            self.__percent['last'] = now
+            if self.__percent['counter']:
+                delstr=""
+                for i in range(0, 200):
+                    delstr+="\b"
+                sys.stdout.write(delstr)
+            if not last:
+                text = "{}/{}".format(self.__percent['counter'], self.__percent['total'])
+                percent = 100*self.__percent['counter']/self.__percent['total']
+                self.debug("{}: ".format(self.__percent['text']), color="blue", tail=False)
+                self.debug("{}".format(text), color="cyan", head=False, tail=False)
+                self.debug(" - {:.1f}%      ".format(percent), color="white", head=False, tail=False)
+            else:
+                self.debug(self.__percent['text'], color="blue", tail=False)
+                self.debug(" ... Done                     ", head=False, color="green")
+            sys.stdout.flush()
+        self.__percent['counter'] += 1
 
     def handle(self, *args, **options):
+
+        # Autoconfigure Debugger
+        self.set_name('CODENERIX-GEODATA')
+        self.set_debug()
+
         # print('Erasing existing data ...')
         # City.objects.all().delete()
         # TimeZone.objects.all().delete()
@@ -172,27 +211,37 @@ class Command(BaseCommand):
             exec('from codenerix_geodata.models import ProvinceGeoName{}'.format(lang))
             exec('from codenerix_geodata.models import CityGeoName{}'.format(lang))
 
-        print('Importing continents ... ',)
+        self.debug('Importing ...', color='yellow', tail=False)
+        self.debug(' Continents', color='purple', head=False, tail=False)
+        self.debug(' Countries Regions Provinces Cities', color='grey', head=False)
         continents = {}
         for lang in LANGUAGES:
             filename = join(data_path, COUNTRY_DATA_FILES[lang])
-            for code, name in continents_lines(filename):
+            lines = list(continents_lines(filename))
+            self.percent_init("    > Prepare data {}".format(lang), len(lines))
+            for code, name in lines:
                 if code not in continents:
                     continents[code] = {
                         'model': Continent(code=code)
                     }
                 continents[code][lang] = name
+                self.percent()
 
-        for code, continent in continents.items():
+        items = continents.items()
+        self.percent_init("    > Link", len(items))
+        for code, continent in items:
             try:
                 model = Continent.objects.get(code=code)
                 continent['model'] = model
             except ObjectDoesNotExist:
                 continent['model'].save()
+            self.percent()
 
         for lang in LANGUAGES:
             model_type = eval('ContinentGeoName{}'.format(lang))
-            for continent in continents.values():
+            items = continents.values()
+            self.percent_init("    > Fill {}".format(lang), len(items))
+            for continent in items:
                 try:
                     model = model_type.objects.get(continent=continent['model'])
                 except ObjectDoesNotExist:
@@ -200,13 +249,18 @@ class Command(BaseCommand):
                     model.continent = continent['model']
                 model.name = continent[lang]
                 model.save()
-        print('Ok')
+                self.percent()
 
-        print('Importing countries ... ',)
+        self.debug('Importing ...', color='yellow', tail=False)
+        self.debug(' Continents', color='simplepurple', head=False, tail=False)
+        self.debug(' Countries', color='purple', head=False, tail=False)
+        self.debug(' Regions Provinces Cities', color='grey', head=False)
         countries = {}
         for lang in LANGUAGES:
             filename = join(data_path, COUNTRY_DATA_FILES[lang])
-            for geoid, continent, code, name in country_lines(filename):
+            items = list(country_lines(filename))
+            self.percent_init("    > Prepare data {}".format(lang), len(items))
+            for geoid, continent, code, name in items:
                 if code not in countries:
                     countries[code] = {
                         'model': Country(
@@ -216,17 +270,22 @@ class Command(BaseCommand):
                         )
                     }
                 countries[code][lang] = name
+                self.percent()
 
-        for country in countries.values():
+        values = countries.values()
+        self.percent_init("    > Link", len(values))
+        for country in values:
             try:
                 model = Country.objects.get(code=country['model'].code)
                 country['model'] = model
             except ObjectDoesNotExist:
                 country['model'].save()
+            self.percent()
 
         for lang in LANGUAGES:
             model_type = eval('CountryGeoName{}'.format(lang))
-            for country in countries.values():
+            self.percent_init("    > Fill {}".format(lang), len(values))
+            for country in values:
                 try:
                     model = model_type.objects.get(country=country['model'])
                 except ObjectDoesNotExist:
@@ -234,13 +293,18 @@ class Command(BaseCommand):
                     model.country = country['model']
                 model.name = country[lang]
                 model.save()
-        print('Ok')
+                self.percent()
 
-        print('Importing regions ... ',)
+        self.debug('Importing ...', color='yellow', tail=False)
+        self.debug(' Continents Countries', color='simplepurple', head=False, tail=False)
+        self.debug(' Regions', color='purple', head=False, tail=False)
+        self.debug(' Provinces Cities', color='grey', head=False)
         regions = {}
         for lang in LANGUAGES:
             filename = join(data_path, CITY_DATA_FILES[lang])
-            for geoid, country_code, region_code, region_name in region_lines(filename):
+            lines = list(region_lines(filename))
+            self.percent_init("    > Prepare data {}".format(lang), len(lines))
+            for geoid, country_code, region_code, region_name in lines:
                 region_key = '{}_{}'.format(country_code, region_code)
                 if region_key not in regions:
                     regions[region_key] = {
@@ -251,19 +315,27 @@ class Command(BaseCommand):
                         )
                     }
                 regions[region_key][lang] = region_name
+                self.percent()
 
-        for region in regions.values():
+        items = regions.values()
+        self.percent_init("    > Link", len(items))
+        for region in items:
             try:
                 model = Region.objects.get(pk=region['model'].pk)
                 region['model'] = model
             except ObjectDoesNotExist:
                 region['model'].save()
+            self.percent()
 
+        self.debug("    > Populate missing", color='blue', tail=False)
         populate_missing_names(regions)
+        self.debug(" ... Done", color='green', head=False)
 
         for lang in LANGUAGES:
             model_type = eval('RegionGeoName{}'.format(lang))
-            for region in regions.values():
+            items = regions.values()
+            self.percent_init("    > Fill {}".format(lang), len(items))
+            for region in items:
                 try:
                     model = model_type.objects.get(region=region['model'])
                 except ObjectDoesNotExist:
@@ -271,13 +343,18 @@ class Command(BaseCommand):
                     model.region = region['model']
                     model.name = region[lang]
                 model.save()
-        print('Ok')
+                self.percent()
 
-        print('Importing provinces ... ',)
+        self.debug('Importing ...', color='yellow', tail=False)
+        self.debug(' Continents Countries Regions', color='simplepurple', head=False, tail=False)
+        self.debug(' Provinces', color='purple', head=False, tail=False)
+        self.debug(' Cities', color='grey', head=False)
         provinces = {}
         for lang in LANGUAGES:
             filename = join(data_path, CITY_DATA_FILES[lang])
-            for geoid, country_code, region_code, province_code, province_name in province_lines(filename):
+            lines = list(province_lines(filename))
+            self.percent_init("    > Prepare data {}".format(lang), len(lines))
+            for geoid, country_code, region_code, province_code, province_name in lines:
                 region_key = '{}_{}'.format(country_code, region_code)
                 province_key = '{}_{}_{}'.format(country_code, region_code, province_code)
                 if province_key not in provinces:
@@ -289,19 +366,27 @@ class Command(BaseCommand):
                         )
                     }
                 provinces[province_key][lang] = province_name
+                self.percent()
 
-        for province in provinces.values():
+        items = provinces.values()
+        self.percent_init("    > Link", len(items))
+        for province in items:
             try:
                 model = Province.objects.get(pk=province['model'].pk)
                 province['model'] = model
             except ObjectDoesNotExist:
                 province['model'].save()
+            self.percent()
 
+        self.debug("    > Populate missing", color='blue', tail=False)
         populate_missing_names(provinces)
+        self.debug(" ... Done", color='green', head=False)
 
         for lang in LANGUAGES:
             model_type = eval('ProvinceGeoName{}'.format(lang))
-            for province in provinces.values():
+            items = provinces.values()
+            self.percent_init("    > Fill {}".format(lang), len(items))
+            for province in items:
                 try:
                     model = model_type.objects.get(province=province['model'])
                 except ObjectDoesNotExist:
@@ -309,14 +394,18 @@ class Command(BaseCommand):
                     model.province = province['model']
                 model.name = province[lang]
                 model.save()
-        print('Ok')
+                self.percent()
 
-        print('Importing cities ... ',)
+        self.debug('Importing ...', color='yellow', tail=False)
+        self.debug(' Continents Countries Regions Provinces', color='simplepurple', head=False, tail=False)
+        self.debug(' Cities', color='purple', head=False, tail=False)
         cities = {}
         timezones = {}
         for lang in LANGUAGES:
             filename = join(data_path, CITY_DATA_FILES[lang])
-            for city_id, country_code, region_code, province_code, city_name, time_zone in city_lines(filename):
+            lines = list(city_lines(filename))
+            self.percent_init("    > Prepare data {}".format(lang), len(lines))
+            for city_id, country_code, region_code, province_code, city_name, time_zone in lines:
                 if time_zone not in timezones:
                     try:
                         model = TimeZone.objects.get(name=time_zone)
@@ -344,19 +433,27 @@ class Command(BaseCommand):
                     }
 
                 cities[city_id][lang] = city_name
+                self.percent()
 
-        for city in cities.values():
+        items = cities.values()
+        self.percent_init("    > Link", len(items))
+        for city in items:
             try:
                 model = City.objects.get(pk=city['model'].pk)
                 city['model'] = model
             except ObjectDoesNotExist:
                 city['model'].save()
+            self.percent()
 
+        self.debug("    > Populate missing", color='blue', tail=False)
         populate_missing_names(cities)
+        self.debug(" ... Done", color='green', head=False)
 
         for lang in LANGUAGES:
             model_type = eval('CityGeoName{}'.format(lang))
-            for city in cities.values():
+            items = cities.values()
+            self.percent_init("    > Fill {}".format(lang), len(items))
+            for city in items:
                 try:
                     model = model_type.objects.get(city=city['model'])
                 except ObjectDoesNotExist:
@@ -364,18 +461,18 @@ class Command(BaseCommand):
                     model.city = city['model']
                 model.name = city[lang]
                 model.save()
-        print('Ok')
+                self.percent()
 
-        print('Removing regions without cities ...',)
+        self.debug('Removing regions without cities ...', color='yellow', tail=False)
         for region in Region.objects.all():
             if region.cities.count() == 0:
                 region.delete()
-        print('Ok')
+        self.debug(" ... Done", color='green', head=False)
 
-        print('Removing provinces without cities ...',)
+        self.debug('Removing provinces without cities ...', color='yellow', tail=False)
         for province in Province.objects.all():
             if province.cities.count() == 0:
                 province.delete()
-        print('Ok')
+        self.debug(" ... Done", color='green', head=False)
 
-        print('All done!!!')
+        self.debug('All done !!!', color="green")
